0f409154a098d7ff1cad31eb5e8ac9edcb1d938e
Mohanavelu <mohanaveluk@gmail.com>
2025-03-19 17:30:57 -0500

    removing dist folder in git

--- dist/order/entities/order-detail.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Product } from '../../product/entities/product.entity';
-import { Order } from './order.entity';
-export declare class OrderDetail {
-    id: string;
-    orderId: string;
-    order: Order;
-    product: Product;
-    quantity: number;
-}
